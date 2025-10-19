41ef7b2a66042106e47b14ffda2bcd50c49edcfa
Kwangsoo Kim <gimgwangsu0@gmail.com>
2025-10-19 19:00:17 +0900

    회원가입 발급 jwt payload 수정

--- src/user-backend/lib/auth/signUp.js
+++ src/user-backend/lib/auth/signUp.js
@@ -119,7 +119,7 @@ module.exports = {
                             
             // 토큰 payload 정의: 민감하지 않은 사용자 고유 정보 포함
             const tokenPayload = {
-                id: sanitizedPost.id,
+                id: newUserId,
                 // role 등의 권한 정보도 여기에 포함 가능
             };
 
