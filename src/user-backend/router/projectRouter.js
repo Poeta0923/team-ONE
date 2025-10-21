df0ea707b5d9d0bcde8eb4d529ce8d95a1f9ffa4
Kwangsoo Kim <gimgwangsu0@gmail.com>
2025-10-22 06:21:42 +0900

    프로젝트 수정 기능 라우팅 설정

--- src/user-backend/router/projectRouter.js
+++ src/user-backend/router/projectRouter.js
@@ -49,7 +49,16 @@ router.post('/create', verifyToken, (req, res)=>{
     create.create(req, res);
 })
 
-// [3] [GET] api/project/detail/:projectId 경로 정의
+// [3] [PATCH] api/project/edit/:projectId 경로 정의
+router.patch('edit:/projectId', verifyToken, (req, res)=>{
+    // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
+    logger.info(`GET /api/project/edit/:projectId - User: ${req/user ? req.user.userid : N/A}`);
+
+    // edit.edit 함수가 실행될 때 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
+    edit.edit(req, res);
+})
+
+// [4] [GET] api/project/detail/:projectId 경로 정의
 router.get('detail/:projectId', verifyToken, (req, res)=>{
     // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
     logger.info(`GET /api/project/detail/:projectId - User: ${req/user ? req.user.userid : N/A}`);
@@ -58,7 +67,7 @@ router.get('detail/:projectId', verifyToken, (req, res)=>{
     detail.detail(req, res);
 })
 
-// [4] [DELETE] api/project/delete/:projectId 경로 정의
+// [5] [DELETE] api/project/delete/:projectId 경로 정의
 router.delete('delete/:projectId', verifyToken, (req, res)=>{
     // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
     logger.info(`DELETE /api/project/delete/:projectId - User: ${req.user ? req.user.userid : 'N/A'}`);
@@ -67,7 +76,7 @@ router.delete('delete/:projectId', verifyToken, (req, res)=>{
     deleteProject.delete(req, res);
 })
 
-// [5] [PATCH] api/project/complete/:projectId 경로 정의
+// [6] [PATCH] api/project/complete/:projectId 경로 정의
 router.patch('/patch/:projectId', verifyToken, (req, res)=>{
     // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
     logger.info(`PATCH /api/project/complete/:projectId - User: ${req.user ? req.user.userid : 'N/A'}`);
