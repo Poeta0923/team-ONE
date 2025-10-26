81062cd80f8d9080bb85a2528969496426d680c2
Kwangsoo Kim <gimgwangsu0@gmail.com>
2025-10-26 22:28:08 +0900

    projectRouter.js 오류수정

--- src/user-backend/router/projectRouter.js
+++ src/user-backend/router/projectRouter.js
@@ -21,13 +21,16 @@ const contest = require('../lib/project/contest');
 // [2] 프로젝트 생성 모듈 호출
 const create = require('../lib/project/create');
 
-// [3] 프로젝트 상세 정보 조회 모듈 호출
+// [3] 프로젝트 수정 모듈 호출
+const edit = require('../lib/project/edit');
+
+// [4] 프로젝트 상세 정보 조회 모듈 호출
 const detail = require('../lib/project/detail');
 
-// [4] 프로젝트 삭제 모듈 호출
+// [5] 프로젝트 삭제 모듈 호출
 const deleteProject = require('../lib/project/delete');
 
-// [5] 프로젝트 완료 처리 모듈 호출
+// [6] 프로젝트 완료 처리 모듈 호출
 const complete = require('../lib/project/complete');
 
 // =================================================================
@@ -43,43 +46,43 @@ router.get('/contest', (req, res)=>{
 // [2] [POST] /api/project/create 경로 정의
 router.post('/create', verifyToken, (req, res)=>{
     // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
-    logger.info(`POST /api/project/create - User: ${req.user ? req.user.userid : 'N/A'}`);
+    logger.info(`POST /api/project/create - User: ${req.user ? req.user.userId : 'N/A'}`);
 
     // create.create 함수가 실행될 때 req.user 객체가 존재함을 보장합니다.
     create.create(req, res);
 })
 
 // [3] [PATCH] api/project/edit/:projectId 경로 정의
-router.patch('edit:/projectId', verifyToken, (req, res)=>{
+router.patch('/edit/:projectId', verifyToken, (req, res)=>{
     // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
-    logger.info(`GET /api/project/edit/:projectId - User: ${req/user ? req.user.userid : N/A}`);
+    logger.info(`GET /api/project/edit/:projectId - User: ${req.user ? req.user.userId : N/A}`);
 
     // edit.edit 함수가 실행될 때 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
     edit.edit(req, res);
 })
 
 // [4] [GET] api/project/detail/:projectId 경로 정의
-router.get('detail/:projectId', verifyToken, (req, res)=>{
+router.get('/detail/:projectId', verifyToken, (req, res)=>{
     // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
-    logger.info(`GET /api/project/detail/:projectId - User: ${req/user ? req.user.userid : N/A}`);
+    logger.info(`GET /api/project/detail/:projectId - User: ${req.user ? req.user.userId : N/A}`);
 
     // detail.detail 함수가 실행될 때 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
     detail.detail(req, res);
 })
 
 // [5] [DELETE] api/project/delete/:projectId 경로 정의
-router.delete('delete/:projectId', verifyToken, (req, res)=>{
+router.delete('/delete/:projectId', verifyToken, (req, res)=>{
     // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
-    logger.info(`DELETE /api/project/delete/:projectId - User: ${req.user ? req.user.userid : 'N/A'}`);
+    logger.info(`DELETE /api/project/delete/:projectId - User: ${req.user ? req.user.userId : 'N/A'}`);
 
     // delete.delete 함수가 실행될 때 req.user 객체가 존재함을 보장합니다.
     deleteProject.delete(req, res);
 })
 
 // [6] [PATCH] api/project/complete/:projectId 경로 정의
-router.patch('/patch/:projectId', verifyToken, (req, res)=>{
+router.patch('/complete/:projectId', verifyToken, (req, res)=>{
     // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
-    logger.info(`PATCH /api/project/complete/:projectId - User: ${req.user ? req.user.userid : 'N/A'}`);
+    logger.info(`PATCH /api/project/complete/:projectId - User: ${req.user ? req.user.userId : 'N/A'}`);
 
     // complete.complete 함수가 실행될 때 req.user 객체가 존재함을 보장합니다.
     complete.complete(req, res);
