00439f6431ca19bcc2bbcec74833ef5be7b1f99e
Presentk <632366@naver.com>
2025-10-26 23:34:56 +0900

    서버 업로드를 위한 설정 수정

--- src/test/java/com/teammatching/admin/auth/controller/PasswordEncodeTest.java
+++ src/test/java/com/teammatching/admin/auth/controller/PasswordEncodeTest.java
@@ -10,7 +10,7 @@ public class PasswordEncodeTest {
     void encodePassword() {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         // 암호화하고 싶은 비밀번호 입력
-        String encodedPassword = passwordEncoder.encode("1234");
+        String encodedPassword = passwordEncoder.encode("admin123");
         System.out.println("암호화된 비밀번호: " + encodedPassword);
     }
 }
