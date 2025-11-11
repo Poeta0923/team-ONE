6ceb1dad9d6bb90c4dc7041b22cfe3ef8c210a38
Kwangsoo Kim <gimgwangsu0@gmail.com>
2025-11-11 17:26:55 +0900

    채팅 목록 조회 기능 라우팅 설정

--- src/user-backend/router/chatRouter.js
+++ src/user-backend/router/chatRouter.js
@@ -21,6 +21,9 @@ const private = require('../lib/chat/private');
 // [2] 채팅 전송 모듈 호출
 const message = require('../lib/chat/message');
 
+// [3] 채팅 목록 조회 모듈 호출
+const list = require ('../lib/chat/list');
+
 // =================================================================
 // 3. API Route Endpoints Definition
 // =================================================================
@@ -41,6 +44,15 @@ router.ws('/message', verifyToken, (ws, res)=>{
 
     // message.message 함수가 실행될 때 req.user 객체가 존재함을 보장합니다.
     message.message(ws, res);
+})
+
+// [3] [GET] /api/chat/list 경로 정의
+router.get('/private', verifyToken, (req, res)=>{
+    // 이 라우트 핸들러는 verifyToken을 성공적으로 통과했을 때만 실행됩니다.
+    logger.info(`GET /api/chat/list - User: ${req.user ? req.user.userId : 'N/A'}`);
+
+    // list.list 함수가 실행될 때 req.user 객체가 존재함을 보장합니다.
+    list.list(req, res);
 })
 
 // 다른 파일(server.js)에서 사용할 수 있도록 router 객체 내보내기
